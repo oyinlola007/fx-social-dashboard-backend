91ed4d94bb2dd5b0ab5d98c999781550f9ab5d53
oyinlola007 <ojelere.oyinlola@babbangona.com>
2022-03-09 02:29:21 +0000

    aaa

--- cogs/config.py
+++ cogs/config.py
@@ -1,7 +1,7 @@
 CURRENCY = "LTC"
 DATE_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
-DATABASE_NAME = "./data/database.db"
-DATABASE_NAME2 = "./data/trial_database.db"
+DATABASE_NAME = "C:/Users/Administrator/Documents/GitHub/subscriptions-and-affiliates/data/database.db"
+DATABASE_NAME2 = "C:/Users/Administrator/Desktop/trial_database.db"
 
 
 
